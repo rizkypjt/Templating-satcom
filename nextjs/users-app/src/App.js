54e8b8d0108f854fa6dde3d43d78857ec150f8f8
rizkypjt <rizkyhidayatpanjaitan97@gmail.com>
2019-12-27 11:36:13 +0700

    add Breadcrumbs and routing

--- nextjs/users-app/src/App.js
+++ nextjs/users-app/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import MenuList from './Components/MenuList';
+// import MenuList from './Components/MenuList';
+import Users from './Components/Users'
 
 
 class App extends Component {
   render() {
     return (
       <div>
-        <MenuList/>
+        {/* <MenuList/> */}
+        <Users/>
      </div>
     )
   }
